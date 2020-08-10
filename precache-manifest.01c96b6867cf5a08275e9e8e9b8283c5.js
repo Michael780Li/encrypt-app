2ea040edb5c7d3beadf72bb24f8a8a407fe4a3ae
Michael <mjfl780@gmail.com>
2020-08-10 01:49:51 -0400

    Updates

---
+++ precache-manifest.01c96b6867cf5a08275e9e8e9b8283c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340d2fc2dcc4a9fa9467a92fda98f2bc",
+    "url": "/encrypt-app/index.html"
+  },
+  {
+    "revision": "66e012341fa5abd5052a",
+    "url": "/encrypt-app/static/css/main.647e8327.chunk.css"
+  },
+  {
+    "revision": "126bc261392ed9205e2f",
+    "url": "/encrypt-app/static/js/2.200fe64e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/encrypt-app/static/js/2.200fe64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e012341fa5abd5052a",
+    "url": "/encrypt-app/static/js/main.2c93001e.chunk.js"
+  },
+  {
+    "revision": "5f681a6d3161c386c77b",
+    "url": "/encrypt-app/static/js/runtime-main.8041981b.js"
+  }
+]);
